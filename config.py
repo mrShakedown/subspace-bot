740acc1a6351ff1ec4eec76e60bf0d3aa643d8d0
mrShakedown <118470343+mrShakedown@users.noreply.github.com>
2023-01-28 20:26:48 +0400

    add descr

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = 'YOUR_TOKEN'
-data_path = '/dev'
-timer = 10
-admin_id = 123 #from https://t.me/my_id_bot
+token = 'YOUR_TOKEN' #bot api-token
+data_path = '/dev' #path to track disk space and alerts
+timer = 10 #alert and check frequency in seconds
+admin_id = 123 #your personal telegram ID from https://t.me/my_id_bot
